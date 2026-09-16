da72881af4b56cd41c1c863f06b593f55bfd6440
stejob22 <stejob22@htl-kaindorf.at>
2026-09-16 11:44:46 +0200

    v1-init & db_connection

---
+++ src/main/java/stejob/exa01_spring_rest/Exa01SpringRestApplication.java
@@ -0,0 +1,13 @@
+package stejob.exa01_spring_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exa01SpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exa01SpringRestApplication.class, args);
+    }
+
+}
