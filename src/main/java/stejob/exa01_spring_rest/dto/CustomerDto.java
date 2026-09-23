26dd9f3a9e9e57302ad74e2ca2478339ed4e4c21
stejob22 <stejob22@htl-kaindorf.at>
2026-09-23 11:25:20 +0200

    record dto added

---
+++ src/main/java/stejob/exa01_spring_rest/dto/CustomerDto.java
@@ -0,0 +1,29 @@
+package stejob.exa01_spring_rest.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public record CustomerDto(
+        Long id,
+        String firstname,
+        String lastname,
+        @JsonFormat(pattern = "dd-MM-yyyy")
+        LocalDate birthday
+) {
+    public CustomerDto {
+        if (birthday.isAfter(LocalDate.now().minusYears(12))) {
+            throw new RuntimeException(("Too young"));
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            CustomerDto customerDto = new CustomerDto(1L, "asa", "asa", LocalDate.now());
+            System.out.println(customerDto);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
