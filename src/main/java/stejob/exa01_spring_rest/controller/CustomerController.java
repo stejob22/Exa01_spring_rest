4135b2c344d17357bc6679b852c7b7f37c9338cc
stejob22 <stejob22@htl-kaindorf.at>
2026-09-23 12:05:11 +0200

    Finally v5

--- src/main/java/stejob/exa01_spring_rest/controller/CustomerController.java
+++ src/main/java/stejob/exa01_spring_rest/controller/CustomerController.java
@@ -3,9 +3,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import stejob.exa01_spring_rest.pojos.dto.CustomerDto;
 import stejob.exa01_spring_rest.services.CustomerService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -28,16 +30,24 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity<CustomerDto> create(@RequestBody CustomerDto customerDto) {
+        checkAge(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customerDto));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CustomerDto> update(@PathVariable Long id, @RequestBody CustomerDto customerDto) {
+        checkAge(customerDto);
         return ResponseEntity.of(customerService.update(id, customerDto));
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<CustomerDto> deleteById(@PathVariable Long id) {
         return ResponseEntity.of(customerService.deleteById(id));
+    }
+
+    private static void checkAge(CustomerDto dto) {
+        if (dto.birthday() == null || dto.birthday().isAfter(LocalDate.now().minusYears(12))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Too young");
+        }
     }
 }
--- src/main/java/stejob/exa01_spring_rest/pojos/dto/CustomerDto.java
+++ src/main/java/stejob/exa01_spring_rest/pojos/dto/CustomerDto.java
@@ -11,19 +11,4 @@ public record CustomerDto(
         @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDate birthday
 ) {
-    public CustomerDto {
-        if (birthday.isAfter(LocalDate.now().minusYears(12))) {
-            throw new RuntimeException(("Too young"));
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            CustomerDto customerDto = new CustomerDto(1L, "asa", "asa", LocalDate.now());
-            System.out.println(customerDto);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
 }
